8593b6eef261e965e88d4ea3cc3db875f6038553
Rodriguez <joaogabrielrdz09@gmail.com>
2024-04-16 20:00:57 -0300

    created the model, url n views for the camera, also a html page for the img

--- octa/views.py
+++ octa/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import cv2
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
 
-# Create your views here.
+@gzip.gzip_page
+def camera_stream(request):
+    cap = cv2.VideoCapture(0)  # 0 para a primeira câmera disponível
+
+    def generate_frames():
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Realize o processamento do frame, se necessário
+
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+            # Encerre o loop se a tecla 'q' for pressionada
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
--- octacity/octacity/urls.py
+++ octacity/octacity/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('camera_stream/', views.camera_stream, name='camera_stream'),
 ]
