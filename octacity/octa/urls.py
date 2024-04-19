ce47d846f4b8d9448322fc94d756076996c22aa6
Rodriguez <joaogabrielrdz09@gmail.com>
2024-04-19 07:54:46 -0300

    camera_stream now on main page

--- octacity/octa/urls.py
+++ octacity/octa/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('camera_stream/', views.camera_stream, name='camera_stream'),
+    path('', views.camera_stream, name='camera_stream'),
 ]
 
 #esse urls eu apenas copiei da octacity, sendo a urls especifica do app octa
