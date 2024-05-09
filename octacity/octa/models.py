cd635dcf0ef5c80bec9aa2553970167ddf9772e4
Rodriguez <joaogabrielrdz09@gmail.com>
2024-05-09 14:37:07 -0300

    mais um atributo

--- octacity/octa/models.py
+++ octacity/octa/models.py
@@ -5,5 +5,6 @@ class Camera(models.Model):
     id = models.CharField(max_length=20)
     ip = models.GenericIPAddressField()
     data = models.DateTimeField() #tem q ver uma maneira de puxar a data atual
+    local = models.TextField()
 
 #aqui que o django nos deixa manipular o bd, como uma criação de tabelas, onde a tabela "Camera" possui as colunas nome e id
