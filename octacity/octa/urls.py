5526dc7398feb9e24a63db3f634d0e5ce782797f
Ian Esteves <pk20lian@gmail.com>
2024-05-24 14:44:50 -0300

    limpando o codigo

--- octacity/octa/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('camera_stream/', views.camera_stream, name='camera_stream'),
-    path('', views.camera_stream, name='camera_stream'), #essa é a página main, principal
-]
-
-#esse urls eu apenas copiei da octacity, sendo a urls especifica do app octa
