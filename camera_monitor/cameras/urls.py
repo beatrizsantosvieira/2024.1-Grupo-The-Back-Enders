e4bad12159a94abb90e505def178f5a7d4d146a7
IAN ESTEVES AMARAL FRANCISCO <202302937314@alunos.ibmec.edu.br>
2024-06-07 17:04:47 -0300

    subindo api

--- camera_monitor/cameras/urls.py
+++ camera_monitor/cameras/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('camera/<int:pk>/edit/', views.camera_update, name='camera_update'),
     path('camera/<int:pk>/delete/', views.camera_delete, name='camera_delete'),
     path('camera/<int:pk>/video_feed/', views.video_feed, name='video_feed'),
+    path('api/', include(router.urls)),
 ]
